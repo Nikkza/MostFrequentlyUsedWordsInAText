01ecfa7cfe13a3a6287d3d38dce47d8e05e0b56e
Nikkza <isaksson.nicklas80@outlook.com>
2020-05-20 12:48:28 +0300

    Added while(true) and Refactoring

--- MostFrequentlyUsedWordsInAText/Program.cs
+++ MostFrequentlyUsedWordsInAText/Program.cs
@@ -8,30 +8,28 @@ namespace MostFrequentlyUsedWordsInAText
     {
         static void Main(string[] args)
         {
-            string text = "In a village of La Mancha, the name of which I have no desire to call to," +
-                  "mind, there lived not long since one of those gentlemen that keep a lance," +
-                  "in the lance-rack, an old buckler, a lean hack, and a greyhound for," +
-                  "coursing. An olla of rather more beef than mutton, a salad on most," +
-                  "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra," +
-                  "on Sundays, made away with three-quarters of his income.";
+            while (true)
+            {
+                string input = Console.ReadLine();
+                var results = Top3(input);
+                Console.WriteLine(results.Count > 0 ? string.Join(",", results) : "NO LETTERS");
 
-            var results = Top3(text);
-            Console.WriteLine(string.Join(",", results));
-            Console.ReadLine();
+            }
         }
 
         public static List<string> Top3(string inputString)
         {
             List<char> delimiterCharsList = new List<char>();
             for (char c = (char)0; c < 255; c++)
+            {
                 if (!(char.IsLetter(c) || c == '\''))
                     delimiterCharsList.Add(c);
+            }
 
             char[] delimiterChars = delimiterCharsList.ToArray();
             var topWords = new List<string>();
-            var containsLetter = inputString.Any(x => char.IsLetter(x));
 
-            if (containsLetter)
+            if (inputString.Any(x => char.IsLetter(x)))
             {
                 topWords = inputString
                     .Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries)
